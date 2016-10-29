3982910186db3b0ec4796d3f8a3efc150f0c20b3
Kurt Lewis <kurt.jlewis15@gmail.com>
2016-10-28 23:44:56 -0400

    Modify to take arguments of classes to run

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -15,19 +15,36 @@ public class MainFrame extends JFrame {
         setTitle("Visualizations");
         setResizable(true);
         setVisible(true);
-        run();
     }
 
-    public void run() {
-        // TODO - put logic here to run multiple and any Visualizations
-        Visualizer test = new Sort();
-        test.setSize(getWidth(), getHeight());
-        add(test);
-        test.start();
-        remove(test);
+    public void run(String[] args) {
+        /* Things that need to happen - 
+         - new instance of class
+         - set size of class to frame width and height
+         - add class to board
+         - call start on class
+         - remove board once start runs
+        */
+        if (args.length == 0) {
+            //fill args with a list of classes in the bin/ folder
+        }
+        for (int i = 0; i < args.length; i++) {
+            try {
+                Visualizer visualization = (Visualizer)ClassLoader.getSystemClassLoader().loadClass(args[i]).newInstance();
+                visualization.setSize(getWidth(), getHeight());
+                add(visualization);
+                visualization.start();
+                remove(visualization);
+            } catch (Exception e) {
+                dispose();
+                System.out.println("ERROR - class " +args[i] + " could not be found. Please ensure it has been compiled.");
+            }
+            
+        }
     }
 
     public static void main(String[] args) {
-        new MainFrame();
+        MainFrame main = new MainFrame();
+        main.run(args);
     }
 }
