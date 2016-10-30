aae8410c94a07f56cc5859f55c146c56e0908138
Noah Bass <bassjnoah@gmail.com>
2016-10-30 11:08:03 -0400

    Add selection sort

---
+++ src/SelectionSort.java
@@ -0,0 +1,42 @@
+/***
+@author - Noah Bass
+***/
+
+public class SelectionSort extends Sort {
+    // Makes one swap per cycle
+    public boolean cycle() {
+        // no action to take if less than 2 in length
+        // finished sorting if the sort index is at the end of the array
+        if (this.array.length < 2 || this.sortIndex == this.array.length - 1) {
+            return true;
+        }
+
+        // get the array minimum element index
+        int minimumIndex = this.sortIndex;
+        for (int i = this.sortIndex; i < this.array.length; i++) {
+            if (this.array[i] < this.array[minimumIndex]) {
+                minimumIndex = i;
+            }
+        }
+
+        // check if the minimum index is the sort index
+        // if so, the element is already in place
+        if (minimumIndex == this.sortIndex) {
+            this.sortIndex++;
+            return false;
+        }
+
+        // swap the first element with the smallest element
+        int temp = this.array[minimumIndex];
+        this.array[minimumIndex] = this.array[sortIndex];
+        this.array[sortIndex] = temp;
+
+        this.sortIndex++;
+
+        return false;
+    }
+
+    public String getTitle() {
+        return "Selection Sort";
+    }
+}
